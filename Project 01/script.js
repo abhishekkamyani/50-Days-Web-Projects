a8f5dff8695aa2830421fa291556a421829b3781
abhishekkamyani <abhishekkumar.bscsf21kdk@iba-suk.edu.pk>
2023-10-19 14:32:29 +0500

    project 01 completed

---
+++ Project 01/script.js
@@ -0,0 +1,18 @@
+let cards = document.querySelectorAll('.card');
+// let texts = document.querySelectorAll('text');
+
+Array.from(cards).forEach((card, indexA) => {
+    card.addEventListener('click', () => {
+        card.classList.add('active-card');
+        card.querySelector('.text').classList.add('active-text');
+
+
+        Array.from(cards).forEach((cardB, indexB) => {
+            if(indexA !== indexB){
+                cardB.classList.remove('active-card');
+                cardB.querySelector('.text').classList.remove('active-text');
+            }
+        })
+
+    })
+})
