98ebafe3d1d51ed7ac231d8f10d9b3d367098bcb
abhishekkamyani <abhishekkumar.bscsf21kdk@iba-suk.edu.pk>
2023-10-22 21:14:47 +0500

    Project 09 completed

---
+++ Project 09/script.js
@@ -0,0 +1,16 @@
+const audios = document.querySelectorAll('audio');
+let currentPlaying = audios[0];
+
+
+audios.forEach(audio => {
+    const btn = document.createElement('button');
+    btn.innerText = audio.src.substring(audio.src.indexOf('s/') + 2, audio.src.indexOf('.m'))
+    
+    btn.addEventListener('click', () => {
+        currentPlaying.pause();
+        currentPlaying.currentTime = 0;
+        currentPlaying = audio;
+        audio.play();
+    });
+    buttons.appendChild(btn);
+})
